4f95817592770163e612c33836067d1e9aa9de03
Olivio Neves <olivioservio@gmail.com>
2025-11-29 11:55:48 -0300

    LandingPage finalizada, lint rodado com sucesso

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.getElementById('sidebar');
+const closeSidebar = document.getElementById('closeSidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.add('open');
+});
+
+closeSidebar.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+});
